4d365173010bf47639d0af93f00a94e3e75f15fb
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2020-10-06 11:21:26 -0300

    Agrega mock para productos

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -5,9 +5,30 @@ const TABLES = [
   { position: { x: 100, y: 150 }, isSelected: false, isOpen: true, name: "2" }
 ];
 
+const PRODUCTS = [
+  {
+    id: 1,
+    title: "Cerveza IPA",
+    price: 100,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Helles_im_Glas-Helles_%28pale_beer%29.jpg/150px-Helles_im_Glas-Helles_%28pale_beer%29.jpg"
+  },
+  {
+    id: 2,
+    title: "Cerveza SCOTCH",
+    price: 100,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Helles_im_Glas-Helles_%28pale_beer%29.jpg/150px-Helles_im_Glas-Helles_%28pale_beer%29.jpg"
+  }
+];
+
 export const handlers = [
   // Handles a GET /tables request
   rest.get("/api/v1/tables", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(TABLES));
+  }),
+
+  rest.get("/api/v1/products", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(PRODUCTS));
   })
 ];
