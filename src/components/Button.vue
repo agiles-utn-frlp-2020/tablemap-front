b700aec31d3e9607cf577f2d14338518ef91d1ee
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2020-10-05 22:55:22 -0300

    Agrega componente boton

---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="px-4 rounded" :class="classes">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["variant"],
+  computed: {
+    classes() {
+      return this.variant === "outline"
+        ? "border-2 border-indigo-600 button--outline"
+        : "bg-indigo-600 text-white py-3";
+    }
+  }
+};
+</script>
+
+<style>
+.button--outline {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+</style>
