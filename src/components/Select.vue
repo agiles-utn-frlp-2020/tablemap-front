4fafc412a4319ffc96e61a4acc74901b77d18ca6
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2020-10-07 18:21:58 -0300

    Fix altura del select

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -9,7 +9,7 @@
         <button
           type="button"
           @click="toggle"
-          class="cursor-pointer relative w-full w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-primary focus:border-primary-400 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+          class="cursor-pointer relative w-full h-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-primary focus:border-primary-400 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
         >
           <div class="flex items-center space-x-3" v-if="modelValue">
             <img
