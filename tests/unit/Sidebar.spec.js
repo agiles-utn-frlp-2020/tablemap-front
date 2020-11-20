47509a73a44bee94b831c9d4e67d0d9de311eead
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2020-11-20 11:19:14 -0300

    Saca try/catch innecesario

--- tests/unit/Sidebar.spec.js
+++ tests/unit/Sidebar.spec.js
@@ -37,66 +37,62 @@ jest.mock("@/services/orders.js", () => {
   };
 });
 
-try {
-  describe("Sidebar", () => {
-    it("Should renders the correct button if selected table is closed", () => {
-      const props = {
-        table: { isOpen: false }
-      };
+describe("Sidebar", () => {
+  it("Should renders the correct button if selected table is closed", () => {
+    const props = {
+      table: { isOpen: false }
+    };
 
-      const wrapper = mount(Sidebar, { props });
-      const button = wrapper.find("button");
-      expect(button.text()).toBe("Abrir mesa");
-    });
-
-    it("Should trigger 'open-table' event when clicked on sidebar button when closed table is selected", async () => {
-      const props = {
-        table: { isOpen: false }
-      };
-
-      const wrapper = mount(Sidebar, { props });
-      const sidebar = wrapper.findComponent(SidebarClosedTableSelected);
-      const button = wrapper.find("button");
-
-      await button.trigger("click");
-
-      const event = sidebar.emitted()["open-table"];
-
-      expect(event).toBeTruthy();
-    });
-
-    it("Should trigger 'close-table' event when clicked on sidebar button when open table is selected", async () => {
-      const props = {
-        table: { isOpen: true }
-      };
-
-      const wrapper = mount(Sidebar, { props });
-      const button = wrapper.find("button[data-test-id=close]");
-
-      await button.trigger("click");
-      const sidebar = wrapper.findComponent(SidebarOpenTableSelected);
-      const event = sidebar.emitted()["close-table"];
-
-      expect(event).toBeTruthy();
-    });
-
-    it("Should renders the correct button if selected table is open", () => {
-      const props = {
-        table: { isOpen: true }
-      };
-
-      const wrapper = mount(Sidebar, { props });
-      const button = wrapper.find("button[data-test-id=close]");
-      expect(button.text()).toBe("Cerrar mesa");
-    });
-
-    it("Should renders a message if no table is selected", () => {
-      const props = {};
-
-      const wrapper = mount(Sidebar, { props });
-      expect(wrapper.text()).toBe("Por favor seleccione una mesa.");
-    });
+    const wrapper = mount(Sidebar, { props });
+    const button = wrapper.find("button");
+    expect(button.text()).toBe("Abrir mesa");
   });
-} catch (e) {
-  console.log(e);
-}
+
+  it("Should trigger 'open-table' event when clicked on sidebar button when closed table is selected", async () => {
+    const props = {
+      table: { isOpen: false }
+    };
+
+    const wrapper = mount(Sidebar, { props });
+    const sidebar = wrapper.findComponent(SidebarClosedTableSelected);
+    const button = wrapper.find("button");
+
+    await button.trigger("click");
+
+    const event = sidebar.emitted()["open-table"];
+
+    expect(event).toBeTruthy();
+  });
+
+  it("Should trigger 'close-table' event when clicked on sidebar button when open table is selected", async () => {
+    const props = {
+      table: { isOpen: true }
+    };
+
+    const wrapper = mount(Sidebar, { props });
+    const button = wrapper.find("button[data-test-id=close]");
+
+    await button.trigger("click");
+    const sidebar = wrapper.findComponent(SidebarOpenTableSelected);
+    const event = sidebar.emitted()["close-table"];
+
+    expect(event).toBeTruthy();
+  });
+
+  it("Should renders the correct button if selected table is open", () => {
+    const props = {
+      table: { isOpen: true }
+    };
+
+    const wrapper = mount(Sidebar, { props });
+    const button = wrapper.find("button[data-test-id=close]");
+    expect(button.text()).toBe("Cerrar mesa");
+  });
+
+  it("Should renders a message if no table is selected", () => {
+    const props = {};
+
+    const wrapper = mount(Sidebar, { props });
+    expect(wrapper.text()).toBe("Por favor seleccione una mesa.");
+  });
+});
