92f5dd3371df6e272372aa45b3dd8cc289e1e705
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2021-07-07 12:55:30 -0300

    fix url base de la api

--- src/services/constants.js
+++ src/services/constants.js
@@ -3,5 +3,5 @@
 
 export const API_URL =
   window.location.origin === "https://table-map.netlify.app"
-    ? "https://table-map.herokuapp.com/"
+    ? "https://table-map.herokuapp.com/api/v1"
     : "http://localhost:8000/api/v1";
