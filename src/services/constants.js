50ab723eeab5a0937fd285d10c069bd720613997
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2021-07-07 12:45:33 -0300

    Agrega variable env para seleccionar el endpoint base de la api

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,6 +1,7 @@
 // MSW
 // export const API_URL = `${window.location.origin}/api/v1`;
 
-export const API_URL = process.env.NETLIFY
-  ? "https://table-map.herokuapp.com/"
-  : "http://localhost:8000/api/v1";
+export const API_URL =
+  window.location.origin === "https://table-map.netlify.app"
+    ? "https://table-map.herokuapp.com/"
+    : "http://localhost:8000/api/v1";
